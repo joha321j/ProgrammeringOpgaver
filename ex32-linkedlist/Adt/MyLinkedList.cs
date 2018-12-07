e649a567bd944fc0af56f53320607f6d8bac886f
Johannes Ehlers Nyholm Thomsen <joha321j@edu.eal.dk>
2018-12-07 09:15:51 +0100

    Implement ex34

--- ex32-linkedlist/Adt/MyLinkedList.cs
+++ ex32-linkedlist/Adt/MyLinkedList.cs
@@ -10,21 +10,37 @@ namespace Adt
     public class MyLinkedList
     {
         private Node Head { get; set; }
+        private Node Tail { get; set; }
         public int Count { get; private set; }
 
         public void Insert(object objData)
         {
             Node nodeToInsert = new Node(objData);
-            nodeToInsert.Next = Head;
-            Head = nodeToInsert;
+
+            if (Head == null)
+            {
+                Head = nodeToInsert;
+            }
+
+            if (Tail != null)
+            {
+                Tail.Next = nodeToInsert;
+                
+            }
+            nodeToInsert.Previous = Tail;
+            Tail = nodeToInsert;
             Count++;
         }
 
         public void Insert(object objData, int index)
         {
-            if (index == 0) 
+            if (index == Count) 
             {
                 Insert(objData);
+            }
+            else if (index == 0)
+            {
+                InsertAtBeginning(objData);
             }
             else
             {
@@ -36,9 +52,19 @@ namespace Adt
                 }
 
                 nodeToInsert.Next = tempNode.Next;
+                nodeToInsert.Previous = tempNode;
                 tempNode.Next = nodeToInsert;
                 Count++;
             }
+        }
+
+        private void InsertAtBeginning(object objData)
+        {
+            Node nodeToInsert = new Node(objData);
+            nodeToInsert.Next = Head;
+            Head = nodeToInsert;
+            Count++;
+
         }
 
         public void Delete()
@@ -81,30 +107,77 @@ namespace Adt
 
         public override string ToString()
         {
-            string value = String.Empty;
+            string value = string.Empty;
             Node tempNode = Head;
             for (int i = 0; i < Count; i++)
             {
-                value += tempNode.Data.ToString() +"\n";
+                value += tempNode.Data +"\n";
                 tempNode = tempNode.Next;
             }
 
             return value;
         }
 
+        public void Reverse()
+        {
+            Node tempNode = Head;
+            for (int i = 0; i < Count - 1; i++)
+            {
+                Node tempNodeTwo = tempNode.Next;
+                Node tempNodeThree = tempNodeTwo.Next;
+                tempNodeTwo.Next = tempNodeTwo.Previous;
+                tempNodeTwo.Previous = tempNodeThree;
+                tempNode.Next = tempNodeTwo.Previous;
+            }
+
+            Head = Tail;
+            Tail = tempNode;
+
+        }
+
+        public void Swap(int i)
+        {
+            Node tempNode = Head;
+            for (int j = 0; j < i; j++)
+            {
+                tempNode = tempNode.Next;
+            }
+            Node tempNodeTwo = tempNode.Next;
+
+            object tempObj = tempNode.Data;
+            tempNode.Data = tempNodeTwo.Data;
+            tempNodeTwo.Data = tempObj;
+
+        }
+
         class Node
         {
-            public Node Next { get; set; }
-            public object Data { get; }
+            internal Node Next { get; set; }
+            internal Node Previous { get; set; }
+            public object Data { get; set; }
 
             public Node(object objData)
             {
                 Data = objData;
                 Next = null;
+                Previous = null;
             }
         }
 
+        public string FremOgTilbage()
+        {
+            string resultString = string.Empty;
+
+            resultString = ToString();
+            Reverse();
+            Node tempNode = Head;
+            Delete();
+
+            resultString += ToString();
+
+            Insert(tempNode.Data, 0);
+
+            return resultString;
+        }
     }
-
-
 }
